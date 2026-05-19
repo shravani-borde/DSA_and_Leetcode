00a48d8dcb96fb636b94908f1244c6fe6494a3c9
Shravani borde <shravaniborde110@gmail.com>
2026-05-19 11:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (77.94%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,15 +1,17 @@
 class Solution {
-    public void sol(int i, int n, String digits, List<String> res, StringBuilder combo, Map<Character, String> maps){
+    private static final String[] MAPPING = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public void sol(int i, int n, String digits, List<String> res, StringBuilder combo){
         if(i == n){
             res.add(combo.toString());
             return;
         }
         else{
-            String letters = maps.get(digits.charAt(i));
+            String letters = MAPPING[digits.charAt(i) - '0'];
             for(char ch: letters.toCharArray())
             {
                 combo.append(ch);
-                sol(i+1, n, digits, res, combo, maps);
+                sol(i+1, n, digits, res, combo);
                 combo.deleteCharAt(combo.length() - 1);
             }
         }
@@ -21,18 +23,7 @@ class Solution {
         if(digits == null || n == 0)
             return res;
         
-        Map<Character, String> maps = new HashMap<>();
-        maps.put('1',"");
-        maps.put('2',"abc");
-        maps.put('3',"def");
-        maps.put('4',"ghi");
-        maps.put('5',"jkl");
-        maps.put('6',"mno");
-        maps.put('7',"pqrs");
-        maps.put('8',"tuv");
-        maps.put('9',"wxyz");
-
-        sol(0, n, digits, res, new StringBuilder(), maps);
+        sol(0, n, digits, res, new StringBuilder());
         return res;
     }
 }
