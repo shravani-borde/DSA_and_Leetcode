4056160e87455e78436f6a94942a60b7d3ce0501
Shravani borde <shravaniborde110@gmail.com>
2026-05-25 23:45:19 +0530

    Time: 6 ms (96.57%) | Memory: 45.1 MB (28.20%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,21 +1,31 @@
+import java.util.*;
+
 class Solution {
+
     public String largestNumber(int[] nums) {
+
         String[] arr = new String[nums.length];
 
-        // Convert to strings once.
-        for (int i = 0; i < nums.length; i++) {
-            arr[i] = String.valueOf(nums[i]);
+        // convert numbers to strings
+        for(int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i] + "";
         }
 
-        // Core comparator:
-        // a comes before b if (a+b) is lexicographically larger than (b+a).
+        // custom sorting
         Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
 
-        // All-zero case.
-        if (arr[0].equals("0")) return "0";
+        // if all are zeros
+        if(arr[0].equals("0")) {
+            return "0";
+        }
 
-        StringBuilder sb = new StringBuilder();
-        for (String s : arr) sb.append(s);
-        return sb.toString();
+        String ans = "";
+
+        // join all strings
+        for(String s : arr) {
+            ans += s;
+        }
+
+        return ans;
     }
 }
