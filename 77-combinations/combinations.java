13d69818ad9fada000dfdea766f685edc48b74e2
Shravani borde <shravaniborde110@gmail.com>
2026-06-01 23:58:45 +0530

    Time: 19 ms (49.84%) | Memory: 97.6 MB (96.85%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,19 +1,23 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        generateCombinations(1, n, k, new ArrayList<Integer>(), result);
+        backtrack(1, n, k, new ArrayList<>(), result);
         return result;
     }
 
-    private void generateCombinations(int start, int n, int k, List<Integer> combination, List<List<Integer>> result) {
-        if (k == 0) {
-            result.add(new ArrayList<>(combination));
+    private void backtrack(int start, int n, int k,
+                           List<Integer> current,
+                           List<List<Integer>> result) {
+
+        if (current.size() == k) {
+            result.add(new ArrayList<>(current));
             return;
         }
-        for (int i = start; i <= n - k + 1; i++) {
-            combination.add(i);
-            generateCombinations(i + 1, n, k - 1, combination, result);
-            combination.remove(combination.size() - 1);
+
+        for (int i = start; i <= n; i++) {
+            current.add(i);
+            backtrack(i + 1, n, k, current, result);
+            current.remove(current.size() - 1);
         }
     }
 }
