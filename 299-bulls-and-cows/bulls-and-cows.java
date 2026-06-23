41bb12c5fc81f11af7f6263fcc8db6b8aa8632e2
Shravani borde <shravaniborde110@gmail.com>
2026-06-23 17:52:26 +0530

    Time: 4 ms (45.64%) | Memory: 44 MB (23.44%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -4,14 +4,10 @@ class Solution {
     int cows = 0;
     int[] numbers = new int[10];
     for (int i = 0; i<secret.length(); i++) {
-        int s = Character.getNumericValue(secret.charAt(i));
-        int g = Character.getNumericValue(guess.charAt(i));
-        if (s == g) bulls++;
+        if (secret.charAt(i) == guess.charAt(i)) bulls++;
         else {
-            if (numbers[s] < 0) cows++;
-            if (numbers[g] > 0) cows++;
-            numbers[s] ++;
-            numbers[g] --;
+            if (numbers[secret.charAt(i)-'0']++ < 0) cows++;
+            if (numbers[guess.charAt(i)-'0']-- > 0) cows++;
         }
     }
     return bulls + "A" + cows + "B";
