19870256df5ebf7b416aa0b8a39c3a4c9e227a7a
Shravani borde <shravaniborde110@gmail.com>
2026-08-06 16:43:46 +0530

    Time: 106 ms (30.83%) | Memory: 77.7 MB (64.68%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -6,6 +6,7 @@ class Solution {
             return heights[0];
         
         int[] pse = new int[n];
+        int[] nse = new int[n];
         Stack<Integer> st = new Stack<>();
 
         //first, prev smallest element
@@ -17,18 +18,22 @@ class Solution {
         }
 
         st.clear();
-        int max = 0;
-
 
         //then, next smaller element
         for(int i = n-1; i >= 0; i--){
             while(st.size() != 0 && heights[st.peek()] >= heights[i])
                 st.pop();
-            
-            int j = st.isEmpty() ? n : st.peek();
-            max = Math.max(max, heights[i] * (j - pse[i] - 1) );
+            nse[i] = st.isEmpty() ? n : st.peek();
             st.push(i);
         }
-        return max;
+
+        int ansMax = 0;
+
+        for(int i = 0; i < n; i++){
+            int j = nse[i] - pse[i] - 1;
+            ansMax = Math.max(ansMax, heights[i] * j );
+        }
+
+        return ansMax;
     }
 }
